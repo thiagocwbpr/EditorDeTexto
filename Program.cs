05d5f32209a60230e99dfb7bc71e2e17a1160085
Thiago Menezes <thiagocwbpr@gmail.com>
2023-01-18 14:34:42 -0300

    Criado a estrutura da aplicação. Criado o Menu com opções ao usuário, como, abrir arquivo, editar e sair. Nessa ideia, utilizei o SWITCH como forma de condição para gerenciar o menu. Seguimos em frente....

--- Program.cs
+++ Program.cs
@@ -1,1 +1,23 @@
 ﻿
+
+Menu();
+
+static void Menu() {
+
+	Console.WriteLine("O que você deseja fazer? \n");
+	Console.WriteLine("1 - Abrir Arquivo");
+	Console.WriteLine("2 - Criar Novo Arquivo");
+	Console.WriteLine("0 - Sair\n");
+	Console.Write("Selecione a opção numerica desejada: ");
+
+short option = short.Parse(Console.ReadLine());
+
+	switch (option) {
+		case 0: System.Environment.Exit(0); break;
+		case 1: Abrir(); break;
+		case 2: Editar(); break;
+	}
+}
+static void Abrir() {}
+
+static void Editar() {}
