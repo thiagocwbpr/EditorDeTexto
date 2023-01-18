711733ba06dd0ad1ece4814a2c847a9a8f294171
Thiago Menezes <thiagocwbpr@gmail.com>
2023-01-18 18:10:34 -0300

    Iniciado a função de editar o arquivo, no caso, é a função responsavel por criar um novo arquivo no editor. Ainda não está finalizado.

--- Program.cs
+++ Program.cs
@@ -1,9 +1,13 @@
 ﻿
-
+using System.Text;
+Console.BackgroundColor = ConsoleColor.DarkMagenta;
+Console.ForegroundColor = ConsoleColor.White;
+Console.Clear();
 Menu();
 
 static void Menu() {
 
+	Console.WriteLine("\n\n **** BLOCO DE NOTAS **** \n\n");
 	Console.WriteLine("O que você deseja fazer? \n");
 	Console.WriteLine("1 - Abrir Arquivo");
 	Console.WriteLine("2 - Criar Novo Arquivo");
@@ -18,6 +22,22 @@ short option = short.Parse(Console.ReadLine());
 		case 2: Editar(); break;
 	}
 }
-static void Abrir() {}
+static void Abrir() {
+}
 
-static void Editar() {}
+static void Editar() {
+	Console.Clear();
+	Console.WriteLine("Digite o texto abaixo ( ESC para sair)");
+	Console.WriteLine("\n\n---------------------\n\n");
+	string text = "";
+	
+	do {
+        
+        text += Console.ReadLine();
+		text += Environment.NewLine;
+	}
+
+	while (Console.ReadKey().Key != ConsoleKey.Escape);
+
+	Console.Write(text);
+}
