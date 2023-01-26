3cd8f30044727ba76e7c64d3ec293ee675b1d8d1
Thiago Menezes <thiagocwbpr@gmail.com>
2023-01-26 13:59:07 -0300

    Feito um novo formato de Menu Principal. Com uma acessibilidade melhorada.

--- Program.cs
+++ Program.cs
@@ -1,43 +1,39 @@
 ﻿
-using System.Text;
-Console.BackgroundColor = ConsoleColor.DarkMagenta;
-Console.ForegroundColor = ConsoleColor.White;
-Console.Clear();
-Menu();
 
 static void Menu() {
 
-	Console.WriteLine("\n\n **** BLOCO DE NOTAS **** \n\n");
-	Console.WriteLine("O que você deseja fazer? \n");
-	Console.WriteLine("1 - Abrir Arquivo");
-	Console.WriteLine("2 - Criar Novo Arquivo");
-	Console.WriteLine("0 - Sair\n");
-	Console.Write("Selecione a opção numerica desejada: ");
+    Console.BackgroundColor = ConsoleColor.DarkMagenta;
+    Console.Clear();
+    Console.Clear();
+    Console.WriteLine("\n\n\n***** BLOCO DE ANOTAÇÕES ******\n\n");
+    Console.WriteLine("1 - CRIAR UMA NOVA NOTA ");
+    Console.WriteLine("2 - CONSULTAR UMA NOTA");
+    Console.WriteLine("3 - ATUALIZAR UMA NOTA");
+    Console.WriteLine("4 - DELETAR UMA NOTA");
+    Console.WriteLine("0 - SAIR\n\n");
+    Console.WriteLine("*************************************\n");
+    Console.Write("\nSELECIONE A OPÇÃO DESEJADA: ");
+    short option = short.Parse(Console.ReadLine());
+    switch (option) {
 
-short option = short.Parse(Console.ReadLine());
-
-	switch (option) {
-		case 0: System.Environment.Exit(0); break;
-		case 1: Abrir(); break;
-		case 2: Editar(); break;
-	}
-}
-static void Abrir() {
-}
-
-static void Editar() {
-	Console.Clear();
-	Console.WriteLine("Digite o texto abaixo ( ESC para sair)");
-	Console.WriteLine("\n\n---------------------\n\n");
-	string text = "";
-	
-	do {
-        
-        text += Console.ReadLine();
-		text += Environment.NewLine;
-	}
-
-	while (Console.ReadKey().Key != ConsoleKey.Escape);
-
-	Console.Write(text);
+        case 0:
+            Console.Clear();
+            Console.WriteLine("\n\nBLOCO DE NOTAS FINALIZADO!\n\n");
+            Environment.Exit(0);
+            break;
+        case 1:
+            Criar();
+            break;
+        case 2:
+            Consultar();
+            break;
+        case 3:
+            Atualizar();
+            break;
+        case 4:
+            Deletar();
+            break;
+        default:
+            break;
+    }
 }
